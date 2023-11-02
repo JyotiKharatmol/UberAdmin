d5a0f66b6d9e8ed5054cc63057683ccd21f8cbaf
JyotiKharatmol <jyotikharatmol3095@gmail.com>
2023-11-02 14:58:35 +0530

    2nd Nov 14:58

--- src/main/java/Resource_Properties/Listeners.java
+++ src/main/java/Resource_Properties/Listeners.java
@@ -1,11 +1,9 @@
 package Resource_Properties;
-
 import java.io.IOException;
 import org.openqa.selenium.WebDriver;
 import org.testng.ITestContext;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
-
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.ExtentTest;
 import com.aventstack.extentreports.Status;
@@ -18,7 +16,9 @@ public class Listeners extends Base_File implements ITestListener
 	@Override
 	public void onTestStart(ITestResult result)
 	{
+		
 		test = extent.createTest(result.getMethod().getMethodName());	
+		
 	}
 
 	@Override
